7d818e1f1177295cc05eb42227d9e5d96e09d023
davidjwp <davidpwjacobs@gmail.com>
2026-07-05 23:33:23 +0200

    added mailhub|ssmtp basic ver

--- controller/html/sign_up.php
+++ controller/html/sign_up.php
@@ -1,34 +1,42 @@
 <?php
 	include ("sign_up.html");
+	
+	var_dump($_POST);
 
-	$ver = 0;
-	if (empty($_POST['username'])) $ver |= 1;
-	if (empty($_POST['password'])) $ver |= 2;
-	if (empty($_POST['email'])) $ver |= 4;
+	if (!isset($_POST['signup'])) {
+		$ver = 0;
+		if (!empty($_POST['username'])) $ver |= 1;
+		if (!empty($_POST['password'])) $ver |= 2;
+		if (!empty($_POST['email'])) $ver |= 4;
+		
+		echo decbin($ver);
+		echo "<b><b>$ver";
+		if ($ver != 7) echo "<script>alert('Error missing Username, password or email');</script>";
+		else {
 
-	echo decbin($ver);
-	echo "<b><b>$ver";
-	if ($ver != 7) echo "<script>alert('Error missing Username, password or email');</script>";
-	else {
-		$token = random_bytes(32);
-		$validate_hash = hash("sha256", $token);
+			$pdo = new PDO(
+				"mysql:host=model;dbname=camagru;charset=utf8",
+				"camagru_admin",
+				"camagru_admin_pass"
+			);
 
-		$LINK = "http://$_SERVER[HTTP_HOST]/verify_email.php?token=" . bin2hex($token);
+			$token = random_bytes(32);
+			$validate_hash = hash("sha256", $token);
+			
+			$LINK = "http://$_SERVER[HTTP_HOST]/verify_email.php?token=" . bin2hex($token);
+			
+			$to = $_POST["email"];
+			$subject = "Camagru email verification";
+			$message = "validate signup with this link\n\n\t$LINK";
+			
+			// echo "<script type='text/javascript'>alert('sending verification mail');</script>";
+			// mail($to, $subject, $message);
+			$result = mail($to, $subject, $message);
+			if (!$result) {
+				echo "<script>alert('Mail failed');</script>";
+    			error_log(error_get_last());
+			}
 
-		$to = $_POST["email"];
-		$subject = "Camagru email verification";
-		$message = "validate signup with this link\n\n\t$LINK";
-
-		// echo "<script type='text/javascript'>alert('sending verification mail');</script>";
-		// mail($to, $subject, $message);
-
-		if (mail($to, $subject, $message)) {
-        echo "<script>alert('Verification email sent!');</script>";
-    } else {
-        echo "<script>alert('Mail failed to send. Check server configuration.');</script>";
-        // Log the error
-        error_log("Mail failed to send to: " . $to);
-    }
-
+		}
 	}
-	$_POST = [];
+	// $_POST = [];
